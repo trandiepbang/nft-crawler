86b5f627566b72ab84f055a65cbf417bacfb3d4a
bang-tran <darklegend2009@gmail.com>
2021-04-10 17:09:43 +0700

    fix: update script

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const makerplace = require('./tasks/makerplace');
-// const opeansea = require('./tasks/opeansea');
-// const nitfygateway = require('./tasks/niftygateway');
-// opeansea.buildAndProcessData('./opeansea.csv', ["itsheyjeli"], 100);
-// nitfygateway.buildAndProcessData('./niftygateway.csv', ['itsheyjeli']);
+const opeansea = require('./tasks/opeansea');
+const nitfygateway = require('./tasks/niftygateway');
+opeansea.buildAndProcessData('./opeansea.csv', ["itsheyjeli"], 100);
+nitfygateway.buildAndProcessData('./niftygateway.csv', ['itsheyjeli']);
 makerplace.buildAndProcessData("./makerplace.csv", ["itsheyjeli"]);
